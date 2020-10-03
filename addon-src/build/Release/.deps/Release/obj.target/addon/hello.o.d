70dcd16409ceb9fed0f080c56609760d7c70dc8c
Dinar <maildinuju@gmai.com>
2020-10-03 18:31:16 +0700

    added addon

---
+++ addon-src/build/Release/.deps/Release/obj.target/addon/hello.o.d
@@ -0,0 +1,21 @@
+cmd_Release/obj.target/addon/hello.o := g++ -o Release/obj.target/addon/hello.o ../hello.cc '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/home/dinar/.cache/node-gyp/14.8.0/include/node -I/home/dinar/.cache/node-gyp/14.8.0/src -I/home/dinar/.cache/node-gyp/14.8.0/deps/openssl/config -I/home/dinar/.cache/node-gyp/14.8.0/deps/openssl/openssl/include -I/home/dinar/.cache/node-gyp/14.8.0/deps/uv/include -I/home/dinar/.cache/node-gyp/14.8.0/deps/zlib -I/home/dinar/.cache/node-gyp/14.8.0/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/addon/hello.o.d.raw   -c
+Release/obj.target/addon/hello.o: ../hello.cc \
+ /home/dinar/.cache/node-gyp/14.8.0/include/node/node.h \
+ /home/dinar/.cache/node-gyp/14.8.0/include/node/v8.h \
+ /home/dinar/.cache/node-gyp/14.8.0/include/node/cppgc/common.h \
+ /home/dinar/.cache/node-gyp/14.8.0/include/node/v8config.h \
+ /home/dinar/.cache/node-gyp/14.8.0/include/node/v8-internal.h \
+ /home/dinar/.cache/node-gyp/14.8.0/include/node/v8-version.h \
+ /home/dinar/.cache/node-gyp/14.8.0/include/node/v8config.h \
+ /home/dinar/.cache/node-gyp/14.8.0/include/node/v8-platform.h \
+ /home/dinar/.cache/node-gyp/14.8.0/include/node/node_version.h
+../hello.cc:
+/home/dinar/.cache/node-gyp/14.8.0/include/node/node.h:
+/home/dinar/.cache/node-gyp/14.8.0/include/node/v8.h:
+/home/dinar/.cache/node-gyp/14.8.0/include/node/cppgc/common.h:
+/home/dinar/.cache/node-gyp/14.8.0/include/node/v8config.h:
+/home/dinar/.cache/node-gyp/14.8.0/include/node/v8-internal.h:
+/home/dinar/.cache/node-gyp/14.8.0/include/node/v8-version.h:
+/home/dinar/.cache/node-gyp/14.8.0/include/node/v8config.h:
+/home/dinar/.cache/node-gyp/14.8.0/include/node/v8-platform.h:
+/home/dinar/.cache/node-gyp/14.8.0/include/node/node_version.h:
---
+++ index.js
@@ -0,0 +1,3 @@
+const addon = require('./addon-src/build/Release/addon');
+
+console.log(addon.hello());
